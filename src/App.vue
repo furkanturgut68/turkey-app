8efa142b5ab3ada597c0b97766ce817ea8fa649e
furkanturgut68 <furkan.turgut@turkuvazinovasyon.com>
2023-11-22 11:10:39 +0300

    turkey city & district

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="background-style">
+    <div style="display: flex; flex-direction: column; justify-content: center;">
+      <h2 style="display: flex; justify-content: center; padding-top: 30px;">Türkiye İlleri Bilgi Sayfası</h2>
+      <div style="display: flex;">
+        <input type="text" placeholder="İl giriniz" v-model="searchCity"
+          style="width: 50%; padding: 20px; display: flex; justify-content: center; margin-top: 20px; margin-left: 380px;">
+        <button @click="searchButton"
+          style="width: 60px; height: 50px; border-radius:7px; background-color: chocolate; border-color: chocolate; color: white; margin-top: 25px; margin-left: 10px;">Ara</button>
+      </div>
+      <span style="display: flex; justify-content: center; margin-top: 20px; font-weight: bold;" v-if="isError">Lütfen geçerli bir il yazın</span>
+    </div>
+    <div style="margin: 20px 500px;" v-if="isVisible">
+      <city-detail :cityDetailList="cityDetailList"></city-detail>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
+import axios from 'axios';
+import CityDetail from './components/CityDetail.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { CityDetail },
+  setup() {
+    const searchCity = ref('');
+    const isVisible = ref(false);
+    const isError = ref(false);
+    const cityDetailList = ref([]);
+    const searchButton = () => {
+      if (searchCity.value !== '') {
+        axios.get("https://turkiyeapi.cyclic.app/api/v1/provinces?name=" + searchCity.value).then((payload) => {
+          cityDetailList.value = payload.data;
+          isVisible.value = true;
+          searchCity.value = "";
+          isError.value = false;
+        }).catch((err) => {
+          if(err){
+            isError.value = true;
+          }
+        });
+      }
+
+    }
+
+    return { searchCity, searchButton, isVisible, cityDetailList,isError }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.background-style {
+  width: 100%;
+  height: 100vh;
+  background-image: url('https://c4.wallpaperflare.com/wallpaper/885/999/821/istanbul-4k-night-turkey-wallpaper-preview.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
---
+++ src/components/CityDetail.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="city-bg">
+    <span style="display: flex; justify-content: center; font-size: 20px; padding: 10px; font-weight: bold;">İl Detayları</span>
+    <span style="font-weight: bold; margin-bottom: 6px;">İl Adı : <span style="font-weight: 500;">{{ cityDetailList.data[0].name }}</span></span>
+    <span style="font-weight: bold; margin-bottom: 6px;">İl Plaka Kodu : <span style="font-weight: 500;">{{ cityDetailList.data[0].id }}</span></span>
+    <span style="font-weight: bold; margin-bottom: 6px;">İl Nüfusu : <span style="font-weight: 500;">{{ cityDetailList.data[0].population.toLocaleString() }}</span></span>
+    <span style="font-weight: bold; margin-bottom: 6px;">İl Metropol Mü : <span style="font-weight: 500;">{{ cityDetailList.data[0].isMetropolitan ? 'Evet' : 'Hayır' }}</span></span>
+    <span style="font-weight: bold; margin-bottom: 6px;">Coğrafi Bölge : <span style="font-weight: 500;">{{ cityDetailList.data[0].region.tr }}</span></span>
+    <span style="font-weight: bold; margin-bottom: 6px;">İl Koordinatları (Lat/Lon) : <span style="font-weight: 500;">{{ cityDetailList.data[0].coordinates.latitude }} / {{ cityDetailList.data[0].coordinates.longitude }}</span></span>
+    <span style="font-weight: bold; margin-bottom: 6px;">İlçe Sayısı : <span style="font-weight: 500;">{{ cityDetailList.data[0].districts.length }}</span></span>
+    <span style="font-weight: bold; margin-bottom: 6px;">İlçe Listesi : </span>
+    <div v-for="districts in cityDetailList.data[0].districts" :key="districts.id">
+        <span style="font-weight: bold; margin-bottom: 6px; margin-right: 20px;">İlçe Adı : <span style="font-weight: 500;">{{districts.name}}</span></span>
+        <span style="font-weight: bold; margin-bottom: 6px;">İlçe Nüfusu : <span style="font-weight: 500;">{{ districts.population.toLocaleString() }}</span></span>
+
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name:"CityDetail",
+    props: {
+        cityDetailList:{
+            required: true
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.city-bg{
+    border-radius: 7px;
+    background: white;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
